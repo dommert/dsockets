a34f79b24336e5254af919b770e2cea10d043091
Dommert <DommertEnterprises@Gmail.com>
2015-07-21 20:06:04 -0700

    dsocket - client

--- dsockets/dsocket_client.py
+++ dsockets/dsocket_client.py
@@ -1,1 +1,16 @@
-__author__ = 'skynet'
+# Dommert Inc - Dsockets
+# URL
+# dsocket_client.py
+
+
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+s.close                     # Close the socket when done
